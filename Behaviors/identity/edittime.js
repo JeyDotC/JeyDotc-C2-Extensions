01b651e432dd5933e3454fdc1a1a79b764dfb60c
jguevara <jeysson.mendivil@aurea.com>
2018-08-31 19:19:45 -0500

    Added identity behavior!

---
+++ Behaviors/identity/edittime.js
@@ -0,0 +1,156 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Identity",			// as appears in 'add behavior' dialog, can be changed as long as "id" stays the same
+		"id":			"Identity",			// this is used to Identity this behavior and is saved to the project; never change it
+		"version":		"1.0",					// (float in x.y format) Behavior version - C2 shows compatibility warnings based on this
+		"description":	"Applies a Name and/or a series of tags to an object and pick those object by Name or Tag.",
+		"author":		"JeyDotC",
+		"help url":		"<your website or a manual entry on Scirra.com>",
+		"category":		"General",				// Prefer to re-use existing categories, but you can set anything here
+		"flags":		0						// uncomment lines to enable flags...
+					//	| bf_onlyone			// can only be added once to an object, e.g. solid
+	};
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])			// a number
+// AddStringParam(label, description [, initial_string = "\"\""])		// a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])			// accepts either a number or string
+// AddCmpParam(label, description)										// combo with equal, not equal, less, etc.
+// AddComboParamOption(text)											// (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])			// a dropdown list parameter
+// AddObjectParam(label, description)									// a button to click and pick an object type
+// AddLayerParam(label, description)									// accepts either a layer number or name (string)
+// AddLayoutParam(label, description)									// a dropdown list with all project layouts
+// AddKeybParam(label, description)										// a button to click and press a key (returns a VK)
+// AddAudioFileParam(label, description)								// a dropdown list with all imported project audio files
+
+////////////////////////////////////////
+// Conditions
+
+// AddCondition(id,					// any positive integer to uniquely Identity this condition
+//				flags,				// (see docs) cf_none, cf_trigger, cf_fake_trigger, cf_static, cf_not_invertible,
+//									// cf_deprecated, cf_incompatible_with_triggers, cf_looping
+//				list_name,			// appears in event wizard list
+//				category,			// category in event wizard list
+//				display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>, and {my} for the current behavior icon & name
+//				description,		// appears in event wizard dialog when selected
+//				script_name);		// corresponding runtime function name
+				
+// example				
+// AddCondition(0, cf_none, "Is moving", "My category", "{my} is moving", "Description for my condition!", "IsMoving");
+AddStringParam("Name", "Instance name");
+AddCondition(0, cf_static, "Is Named", "Name", "{my} is named {0}", "Pick instances by their name.", "PickByName");
+
+AddStringParam("Tag", "A tag to look for");
+AddCondition(1, cf_static, "Has Tag", "Tag", "{my} has tag {0}", "Pick all instances that contain the given tag.", "PickByTag");
+
+AddStringParam("Tag", "The tag that has been added.");
+AddCondition(2, cf_trigger, "On tagged", "Tag triggers", "{my} has been tagged with <b>{0}</b>", "Triggered when the given tag is added", "OnTagged");
+
+AddStringParam("Tag", "The tag that has been removed.");
+AddCondition(3, cf_trigger, "On un-tagged", "Tag triggers", "{my} tag <b>{0}</b> has been removed", "Triggered when the given tag is removed", "OnUnTagged");
+
+////////////////////////////////////////
+// Actions
+
+// AddAction(id,				// any positive integer to uniquely Identity this action
+//			 flags,				// (see docs) af_none, af_deprecated
+//			 list_name,			// appears in event wizard list
+//			 category,			// category in event wizard list
+//			 display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//			 description,		// appears in event wizard dialog when selected
+//			 script_name);		// corresponding runtime function name
+
+// example
+// AddAction(0, af_none, "Stop", "My category", "Stop {my}", "Description for my action!", "Stop");
+AddStringParam("Name", "The name this instance will have. We recomend it to be unique, though, it's not mandatory.");
+AddAction(0, af_none, "Set Name", "Name", "Set {my} name to {0}", "Sets this instance name", "SetName");
+
+AddStringParam("Tag", "The tag name to add");
+AddAction(1, af_none, "Add Tag", "Tags", "Add {0} tag to {my}", "Adds a tag to this instance", "AddTag");
+
+AddStringParam("Tag", "The tag name to remove");
+AddAction(2, af_none, "Remove Tag", "Tags", "Remove {0} tag from {my}", "Removes a tag from this instance", "RemoveTag");
+
+////////////////////////////////////////
+// Expressions
+
+// AddExpression(id,			// any positive integer to uniquely Identity this expression
+//				 flags,			// (see docs) ef_none, ef_deprecated, ef_return_number, ef_return_string,
+//								// ef_return_any, ef_variadic_parameters (one return flag must be specified)
+//				 list_name,		// currently ignored, but set as if appeared in event wizard
+//				 category,		// category in expressions panel
+//				 exp_name,		// the expression name after the dot, e.g. "foo" for "myobject.foo" - also the runtime function name
+//				 description);	// description in expressions panel
+
+// example
+AddExpression(0, ef_return_string, "Name", "Name", "Name", "Return the name given to this instance.");
+
+AddStringParam("Tag", "The tag name to find");	// a string
+AddExpression(1, ef_return_number, "Has tag", "Tags", "HasTag", "Returns 1 if this instance has the given tag, 0 otherwise");
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+// new cr.Property(ept_integer,		name,	initial_value,	description)		// an integer value
+// new cr.Property(ept_float,		name,	initial_value,	description)		// a float value
+// new cr.Property(ept_text,		name,	initial_value,	description)		// a string
+// new cr.Property(ept_combo,		name,	"Item 1",		description, "Item 1|Item 2|Item 3")	// a dropdown list (initial_value is string of initially selected item)
+
+var property_list = [
+	new cr.Property(ept_text, 	"Name",		"",		"A name for this object."),
+	new cr.Property(ept_text, 	"Tags",		"",		"A list of tags. Separate separator"),
+	new cr.Property(ept_text, 	"Tags separator",		",",		"The separator for this object's tags.")
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ Behaviors/identity/runtime.js
@@ -0,0 +1,333 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Identity = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Identity.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+		
+		this.name = '';
+		this.tags = [];
+		
+		this._addedTag = '';
+		this._removedTag = '';
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+		// Load properties
+		this.name = this.properties[0];
+		
+		var tagsString = this.properties[1];
+		var tagsSeparator = this.properties[2];
+		
+		if(tagsString){
+			if(tagsSeparator){
+				this.tags = tagsString.split(tagsSeparator);
+			}else{
+				this.tags = [ tagsString ];
+			}
+		}
+		// object is sealed after this call, so make sure any properties you'll ever need are created, e.g.
+		// this.myValue = 0;
+	};
+	
+	behinstProto.onDestroy = function ()
+	{
+		// called when associated object is being destroyed
+		// note runtime may keep the object and behavior alive after this call for recycling;
+		// release, recycle or reset any references here as necessary
+	};
+	
+	// called when saving the full state of the game
+	behinstProto.saveToJSON = function ()
+	{
+		// return a Javascript object containing information about your behavior's state
+		// note you MUST use double-quote syntax (e.g. "property": value) to prevent
+		// Closure Compiler renaming and breaking the save format
+		return {
+			// e.g.
+			"n": this.name,
+			"t": this.tags
+		};
+	};
+	
+	// called when loading the full state of the game
+	behinstProto.loadFromJSON = function (o)
+	{
+		// load from the state previously saved by saveToJSON
+		// 'o' provides the same object that you saved, e.g.
+		// this.myValue = o["myValue"];
+		// note you MUST use double-quote syntax (e.g. o["property"]) to prevent
+		// Closure Compiler renaming and breaking the save format
+		this.name = o["n"];
+		this.tags = o["t"];
+	};
+
+	behinstProto.tick = function ()
+	{
+		var dt = this.runtime.getDt(this.inst);
+		
+		// called every tick for you to update this.inst as necessary
+		// dt is the amount of time passed since the last tick, in case it's a movement
+	};
+	
+	// The comments around these functions ensure they are removed when exporting, since the
+	// debugger code is no longer relevant after publishing.
+	/**BEGIN-PREVIEWONLY**/
+	behinstProto.getDebuggerValues = function (propsections)
+	{
+		// Append to propsections any debugger sections you want to appear.
+		// Each section is an object with two members: "title" and "properties".
+		// "properties" is an array of individual debugger properties to display
+		// with their name and value, and some other optional settings.
+		propsections.push({
+			"title": this.type.name,
+			"properties": [
+				// Each property entry can use the following values:
+				// "name" (required): name of the property (must be unique within this section)
+				// "value" (required): a boolean, number or string for the value
+				// "html" (optional, default false): set to true to interpret the name and value
+				//									 as HTML strings rather than simple plain text
+				// "readonly" (optional, default false): set to true to disable editing the property
+				{"name": "Name", "value": this.name},
+				{"name": "Tags", "value": this.tags.join(',')}
+			]
+		});
+	};
+	
+	behinstProto.onDebugValueEdited = function (header, name, value)
+	{
+		// Called when a non-readonly property has been edited in the debugger. Usually you only
+		// will need 'name' (the property name) and 'value', but you can also use 'header' (the
+		// header title for the section) to distinguish properties with the same name.
+		if (name === "Name")
+			this.name = value;
+		
+		if (name === "Tags")
+			this.tags = value.split(',');
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	// the example condition
+	Cnds.prototype.IsMoving = function ()
+	{
+		// ... see other behaviors for example implementations ...
+		return false;
+	};
+	
+	// ... other conditions here ...
+	
+	// static condition
+	Cnds.prototype.PickByName = function (name)
+	{
+		return PickBy.call(this, inst => { 
+			var behaviorInstance = GetIdentityBehavior(inst);
+			return behaviorInstance.name == name;
+		});
+	};
+	
+	Cnds.prototype.PickByTag = function (tag)
+	{
+		return PickBy.call(this, inst => { 
+			var behaviorInstance = GetIdentityBehavior(inst);
+			return behaviorInstance.tags.indexOf(tag) !== -1;
+		});
+	};
+	
+	Cnds.prototype.OnTagged = function(tag) {
+		var result = this._addedTag && this._addedTag == tag;
+		
+		if(result)
+			this._addedTag = '';
+		
+		return result;
+	};
+	
+	Cnds.prototype.OnUnTagged = function(tag) {
+		var result = this._removedTag && this._removedTag == tag;
+		
+		if(result)
+			this._removedTag = '';
+		
+		return result;
+	};
+	
+	function GetIdentityBehavior(inst){
+		
+		for(var index in inst.behavior_insts){
+			var behaviorInstance = inst.behavior_insts[index];
+			
+			if(behaviorInstance.behavior instanceof cr.behaviors.Identity){
+				return behaviorInstance;
+			}
+		}
+		
+		return undefined;
+	};
+	
+	function PickBy(condition){
+		var i, len, j, inst, families, instances, sol;
+		var cnd = this.runtime.getCurrentCondition();
+		
+		sol = this.objtype.getCurrentSol();
+		
+		if (sol.select_all)
+		{
+			sol.select_all = false;
+			cr.clearArray(sol.instances);
+			cr.clearArray(sol.else_instances);
+			
+			instances = this.objtype.instances;
+			
+			for (i = 0, len = instances.length; i < len; i++)
+			{
+				inst = instances[i];
+				
+				if (condition(inst)){
+					if(!cnd.inverted)
+						sol.instances.push(inst);
+					else
+						sol.else_instances.push(inst);
+				}
+				else{
+					if(!cnd.inverted)
+						sol.else_instances.push(inst);
+					else
+						sol.instances.push(inst);
+				}
+			}
+		}
+		else
+		{
+			for (i = 0, j = 0, len = sol.instances.length; i < len; i++)
+			{
+				inst = sol.instances[i];
+				sol.instances[j] = inst;
+				
+				if(!cnd.inverted){
+					if (!condition(inst)){
+						sol.else_instances.push(inst);
+					}
+					else {
+						j++;
+					}
+				} else {
+					if (condition(inst)){
+						sol.else_instances.push(inst);
+					}
+					else {
+						j++;
+					}
+				}
+			}
+			
+			cr.truncateArray(sol.instances, j);
+		}			
+		
+		this.objtype.applySolToContainer();
+		return !!sol.instances.length;
+	};
+	
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	Acts.prototype.SetName = function (name)
+	{
+		this.name = name;
+	};
+	
+	Acts.prototype.AddTag = function (tag)
+	{
+		if(this.tags.indexOf(tag) === -1){
+			this.tags.push(tag);
+			this._addedTag = tag;
+			this.runtime.trigger(cr.behaviors.Identity.prototype.cnds.OnTagged, this.inst);
+		}
+	};
+	
+	Acts.prototype.RemoveTag = function (tag)
+	{
+		var index = this.tags.indexOf(tag);
+		
+		if(index !== -1){
+			cr.arrayRemove(this.tags, index);
+			this._removedTag = tag;
+			this.runtime.trigger(cr.behaviors.Identity.prototype.cnds.OnUnTagged, this.inst);
+		}
+	};
+	
+	// ... other actions here ...
+	
+	behaviorProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+
+	// the example expression
+	/*Exps.prototype.MyExpression = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
+	{
+		ret.set_int(1337);				// return our value
+		// ret.set_float(0.5);			// for returning floats
+		// ret.set_string("Hello");		// for ef_return_string
+		// ret.set_any("woo");			// for ef_return_any, accepts either a number or string
+	};*/
+	
+	Exps.prototype.Name = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
+	{
+		ret.set_string(this.name);		// for ef_return_string
+	};
+	
+	Exps.prototype.HasTag = function (ret, tag)	// 'ret' must always be the first parameter - always return the expression's result through it!
+	{
+		ret.set_int(this.tags.indexOf(tag) !== -1 ? 1 : 0);
+	};
+	
+	// ... other expressions here ...
+	
+	behaviorProto.exps = new Exps();
+	
+}());
