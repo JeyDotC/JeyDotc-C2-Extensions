1330131342b673925a4a9d70ac3affdcacaf9c85
jguevara <jeysson.mendivil@aurea.com>
2018-09-12 08:09:56 -0500

    Created circular list plugin

---
+++ Plugins/circular_list/edittime.js
@@ -0,0 +1,223 @@
+﻿function GetPluginSettings()
+{
+    return {
+        "name":         "Circular List",                // as appears in 'insert object' dialog, can be changed as long as "id" stays the same
+        "id":           "circular_list",                // this is used to identify this plugin and is saved to the project; never change it
+        "version":      "1.0",                  // (float in x.y format) Plugin version - C2 shows compatibility warnings based on this
+        "description":  "Implentation of a circular list data structure",
+        "author":       "JeyDotC",
+        "help url":     " ",
+        "category":     "Data & Storage",               // Prefer to re-use existing categories, but you can set anything here
+        "type":         "object",               // either "world" (appears in layout and is drawn), else "object"
+        "rotatable":    false,                  // only used when "type" is "world".  Enables an angle property on the object.
+        "flags":        0                       // uncomment lines to enable flags...
+                    //  | pf_singleglobal       // exists project-wide, e.g. mouse, keyboard.  "type" must be "object".
+                    //  | pf_texture            // object has a single texture (e.g. tiled background)
+                    //  | pf_position_aces      // compare/set/get x, y...
+                    //  | pf_size_aces          // compare/set/get width, height...
+                    //  | pf_angle_aces         // compare/set/get angle (recommended that "rotatable" be set to true)
+                    //  | pf_appearance_aces    // compare/set/get visible, opacity...
+                    //  | pf_tiling             // adjusts image editor features to better suit tiled images (e.g. tiled background)
+                    //  | pf_animations         // enables the animations system.  See 'Sprite' for usage
+                    //  | pf_zorder_aces        // move to top, bottom, layer...
+                    //  | pf_nosize             // prevent resizing in the editor
+                    //  | pf_effects            // allow WebGL shader effects to be added
+                    //  | pf_predraw            // set for any plugin which draws and is not a sprite (i.e. does not simply draw
+                                                // a single non-tiling image the size of the object) - required for effects to work properly
+    };
+};
+
+////////////////////////////////////////
+// Parameter types:
+// AddNumberParam(label, description [, initial_string = "0"])          // a number
+// AddStringParam(label, description [, initial_string = "\"\""])       // a string
+// AddAnyTypeParam(label, description [, initial_string = "0"])         // accepts either a number or string
+// AddCmpParam(label, description)                                      // combo with equal, not equal, less, etc.
+// AddComboParamOption(text)                                            // (repeat before "AddComboParam" to add combo items)
+// AddComboParam(label, description [, initial_selection = 0])          // a dropdown list parameter
+// AddObjectParam(label, description)                                   // a button to click and pick an object type
+// AddLayerParam(label, description)                                    // accepts either a layer number or name (string)
+// AddLayoutParam(label, description)                                   // a dropdown list with all project layouts
+// AddKeybParam(label, description)                                     // a button to click and press a key (returns a VK)
+// AddAnimationParam(label, description)                                // a string intended to specify an animation name
+// AddAudioFileParam(label, description)                                // a dropdown list with all imported project audio files
+
+////////////////////////////////////////
+// Conditions
+
+// AddCondition(id,                 // any positive integer to uniquely identify this condition
+//              flags,              // (see docs) cf_none, cf_trigger, cf_fake_trigger, cf_static, cf_not_invertible,
+//                                  // cf_deprecated, cf_incompatible_with_triggers, cf_looping
+//              list_name,          // appears in event wizard list
+//              category,           // category in event wizard list
+//              display_str,        // as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//              description,        // appears in event wizard dialog when selected
+//              script_name);       // corresponding runtime function name
+                
+// example              
+AddCondition(0, cf_none, "Is empty", "General", "Is empty", "Check if the list is empty", "IsEmpty");
+
+AddCmpParam("Comaprison", "How to compare the count to");
+AddNumberParam("Number", "Number to compare the count to");
+AddCondition(1, cf_none, "Compare count", "General", "Compare count", "Compare a number with the count of the list", "CompareCount");
+
+AddCmpParam("Comaprison", "How to compare the current element to");
+AddAnyTypeParam("Element", "Value to compare the current to");
+AddCondition(2, cf_none, "Compare current", "Cursor", "Compare current", "Compare a number with the current of the list", "Comparecurrent");
+
+////////////////////////////////////////
+// Actions
+
+// AddAction(id,                // any positive integer to uniquely identify this action
+//           flags,             // (see docs) af_none, af_deprecated
+//           list_name,         // appears in event wizard list
+//           category,          // category in event wizard list
+//           display_str,       // as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//           description,       // appears in event wizard dialog when selected
+//           script_name);      // corresponding runtime function name
+
+// example
+AddNumberParam("Width", "The new number of elements on list.", "1");
+AddAction(0, af_none, "Set size", "Circular list", "Set size to (<i>{0}</i>)", "Set the number of elements in the list.", "SetSize");
+
+AddAction(1, af_none, "Next", "Cursor", "Move the cursor to next element.", "Moves the cursor to the next element in the list. If it gets to the end, it will start again from index 0", "CursorNext");
+AddAction(2, af_none, "Previous", "Cursor", "Move the cursor to previous element.", "Moves the cursor to the previous element in the list. If it gets to the beginning, it will start again from the last element.", "CursorPrevious");
+
+AddNumberParam("Index", "The index at where to put the cursor.", "0");
+AddAction(3, af_none, "Put cursor at", "Cursor", "Put the cursor at <i>{0}</i>.", "Moves the cursor to the given index.", "CursorAt");
+
+AddAnyTypeParam("Value", "The value to look for.", "0");
+AddAction(4, af_none, "Put cursor at the value", "Cursor", "Put the cursor at the index of <i>{0}</i>.", "Tries to move the cursor to the index of the given value.", "CursorAtValue");
+
+AddNumberParam("Index", "The index (0-based) of the list value to set.", "0");
+AddAnyTypeParam("Value", "The value to store in the list.", "0");
+AddAction(5, af_none, "Set value at Index", "Manipulation", "Set value at <i>{0}</i> to <i>{1}</i>", "Set the value at a position in the list.", "SetValue");
+
+AddComboParamOption("back");
+AddComboParamOption("front");
+AddComboParam("Where", "Whether to insert at the beginning or the end of the list.");
+AddAnyTypeParam("Value", "The value to insert to the list.", "0");
+AddAction(6, af_none, "Push", "Manipulation", "Push {0} <i>{1}</i>", "Add a new element to the front or back of the list.", "Push");
+
+AddComboParamOption("back");
+AddComboParamOption("front");
+AddComboParam("Where", "Whether to remove at the beginning or the end of the list.");
+AddAction(7, af_none, "Pop", "Manipulation", "Pop {0}", "Remove an element from the front or back of the list.", "Pop");
+
+AddAction(8, af_none, "Clear", "General", "Clear", "Clears the list", "Clear");
+
+AddAction(9, af_none, "Sort", "Manipulation", "Sort", "Sorts the list", "Sort");
+
+AddComboParamOption("Do not allow repeated values");
+AddComboParamOption("Allow repeated values");
+AddComboParam("Distinct", "Whether to allow repeated values or not.");
+AddAction(10, af_none, "Distinct", "General", "{0}", "Allow/disallow repeated values in the list.", "SetDistinct");
+
+////////////////////////////////////////
+// Expressions
+
+// AddExpression(id,            // any positive integer to uniquely identify this expression
+//               flags,         // (see docs) ef_none, ef_deprecated, ef_return_number, ef_return_string,
+//                              // ef_return_any, ef_variadic_parameters (one return flag must be specified)
+//               list_name,     // currently ignored, but set as if appeared in event wizard
+//               category,      // category in expressions panel
+//               exp_name,      // the expression name after the dot, e.g. "foo" for "myobject.foo" - also the runtime function name
+//               description);  // description in expressions panel
+
+// example
+AddExpression(0, ef_return_any, "Get value at cursor", "Cursor", "Current", "Get the current value from the list.");
+
+AddExpression(1, ef_return_any, "Peek Next", "Cursor", "PeekNext", "Peeks the next element in the list. (Without moving the cursor.)");
+AddExpression(2, ef_return_any, "Peek Previous", "Cursor", "PeekPrevious", "Peeks the previous element in the list. (Without moving the cursor.)");
+
+AddExpression(3, ef_return_number, "Get size", "Circular list", "Size", "Get the number of elements of the list.");
+
+AddExpression(4, ef_return_any, "Front value", "Circular list", "Front", "Get the front value on the list.");
+AddExpression(5, ef_return_any, "Back value", "Circular list", "Back", "Get the back value on the list.");
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+// new cr.Property(ept_integer,  name,   initial_value,  description)        // an integer value
+// new cr.Property(ept_float,    name,   initial_value,  description)        // a float value
+// new cr.Property(ept_text,     name,   initial_value,  description)        // a string
+// new cr.Property(ept_color,    name,   initial_value,  description)        // a color dropdown
+// new cr.Property(ept_font,     name,   "Arial,-16",    description)        // a font with the given face name and size
+// new cr.Property(ept_combo,    name,   "Item 1",       description, "Item 1|Item 2|Item 3")    // a dropdown list (initial_value is string of initially selected item)
+// new cr.Property(ept_link,     name,   link_text,      description, "currentonly")     // has no associated value; simply calls "OnPropertyChanged" on click
+
+var property_list = [
+    new cr.Property(ept_integer, "Width",            10,         "Initial number of elements on the list."),
+    new cr.Property(ept_combo,   "Distinct Values",  "No",       "Allow unique values only (repeated values will be ignored).", "Yes|No"),
+];
+    
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+    return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+    assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+    return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+    assert2(this instanceof arguments.callee, "Constructor called as a function");
+    
+    // Save the constructor parameters
+    this.instance = instance;
+    this.type = type;
+    
+    // Set the default property values from the property table
+    this.properties = {};
+    
+    for (var i = 0; i < property_list.length; i++)
+        this.properties[property_list[i].name] = property_list[i].initial_value;
+        
+    // Plugin-specific variables
+    // this.myValue = 0...
+}
+
+// Called when inserted via Insert Object Dialog for the current time
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+// Called when double clicked in layout
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called after a property has been changed in the properties bar
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+    if (this.properties["Width"] < 0)
+        this.properties["Width"] = 0;
+}
+
+// For rendered objects to load fonts or textures
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+
+// Called to draw self in the editor if a layout object
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// For rendered objects to release fonts or textures
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
---
+++ Plugins/circular_list/runtime.js
@@ -0,0 +1,388 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+//          vvvvvvvv
+cr.plugins_.circular_list = function(runtime)
+{
+    this.runtime = runtime;
+};
+
+(function ()
+{
+    /////////////////////////////////////
+    // *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+    //                            vvvvvvvv
+    var pluginProto = cr.plugins_.circular_list.prototype;
+        
+    /////////////////////////////////////
+    // Object type class
+    pluginProto.Type = function(plugin)
+    {
+        this.plugin = plugin;
+        this.runtime = plugin.runtime;
+    };
+
+    var typeProto = pluginProto.Type.prototype;
+
+    // called on startup for each object type
+    typeProto.onCreate = function()
+    {
+    };
+
+    /////////////////////////////////////
+    // Instance class
+    pluginProto.Instance = function(type)
+    {
+        this.type = type;
+        this.runtime = type.runtime;
+        
+        // any other properties you need, e.g...
+        // this.myValue = 0;
+    };
+    
+    var instanceProto = pluginProto.Instance.prototype;
+
+    // called whenever an instance is created
+    instanceProto.onCreate = function()
+    {
+        var width = this.properties[0];
+        
+        this.distinct = (this.properties[1] !== 0);
+        this.current = 0;
+        this.list = [];
+        
+        this.list.length = width;
+        for (var index = 0; index < width; index++)
+        {
+            if (cr.is_undefined(this.list[index]))
+                this.list[index] = 0;
+        }
+    };
+    
+    instanceProto.nextPosition = function(){
+        var next = this.current + 1;
+        
+        if(next < this.list.length){
+            return next;
+        }
+        
+        return 0;
+    };
+    
+    instanceProto.previousPosition = function(){
+        var previous = this.current - 1;
+        
+        if(previous >= 0){
+            return previous;
+        }else if(this.list.length > 0) {
+            return this.list.length - 1;
+        }
+        
+        return 0;
+    };
+    
+    instanceProto.onDestroy = function ()
+    {
+        this.current = 0;
+        cr.clearArray(this.list);
+    };
+    
+    // only called if a layout object - draw to a canvas 2D context
+    instanceProto.draw = function(ctx)
+    {
+    };
+    
+    // only called if a layout object in WebGL mode - draw to the WebGL context
+    // 'glw' is not a WebGL context, it's a wrapper - you can find its methods in GLWrap.js in the install
+    // directory or just copy what other plugins do.
+    instanceProto.drawGL = function (glw)
+    {
+    };
+    
+    instanceProto.saveToJSON = function ()
+    {
+        return {
+            "d": this.distinct,
+            "c": this.current,
+            "l": this.list
+        };
+    };
+    
+    instanceProto.loadFromJSON = function (o)
+    {
+        this.distinct = o["d"];
+        this.current = o["c"];
+        this.list = o["l"];
+    };
+    
+    /**BEGIN-PREVIEWONLY**/
+    instanceProto.getDebuggerValues = function (propsections)
+    {
+        propsections.push({
+            "title": this.type.name,
+            "properties": [
+                {"name": "Current", "value": this.current},
+                {"name": "Distinct", "value": this.distinct},
+                {"name": "Data", "value": this.list.join(',')}
+            ]
+        });
+    };
+
+    instanceProto.onDebugValueEdited = function (header, name, value)
+    {
+        if (name === "Current")
+            this.current = value;
+        
+        if (name === "Distinct")
+            this.distinct = !!value;
+        
+        if (name === "Data")
+            this.list = value.split(',');
+    };
+    /**END-PREVIEWONLY**/
+
+    //////////////////////////////////////
+    // Conditions
+    function Cnds() {};
+
+    // the example condition
+    Cnds.prototype.IsEmpty = function ()
+    {
+        return (this.list.length == 0);
+    };
+
+    Cnds.prototype.CompareCount = function (cmp, val)
+    {
+        return cr.do_cmp(this.list.length, cmp, val);
+    };
+
+    Cnds.prototype.Comparecurrent = function (cmp, val)
+    {
+        return cr.do_cmp(this.list[current], cmp, val);
+    };
+
+    // ... other conditions here ...
+    
+    pluginProto.cnds = new Cnds();
+    
+    //////////////////////////////////////
+    // Actions
+    function Acts() {};
+
+    // the example action
+    Acts.prototype.SetSize = function (width)
+    {
+        if(width < 0){
+            width = 0;
+        }
+        
+        if(this.list.length === width){
+            return;
+        }
+        
+        this.list.length = width;
+        
+        for (var index = 0; index < width; index++)
+        {
+            if (cr.is_undefined(this.list[index]))
+                this.list[index] = 0;
+        }
+        
+        if(this.current >= width){
+            if(width > 0)
+                this.current = width - 1;
+            else
+                this.current = 0;
+        }
+    };
+    
+    Acts.prototype.CursorNext = function ()
+    {
+        this.current = this.nextPosition();
+    };
+    
+    Acts.prototype.CursorPrevious = function ()
+    {
+        this.current = this.previousPosition();
+    };
+    
+    Acts.prototype.CursorAt = function (index)
+    {
+        if(index < 0){
+            console.warning("Trying to move the cursor to a less than 0 index.");
+            return;
+        }
+        
+        if(index >= this.list.length){
+            console.warning("Trying to move the cursor beyond list size.");
+            return;
+        }
+        
+        this.current = index;
+    };
+    
+    Acts.prototype.CursorAtValue = function (value)
+    {
+        for (var index = 0; index < this.list.length; index++){
+            if(this.list[index] === value){
+                this.current = index;
+                return;
+            }
+        }
+    };
+    
+    Acts.prototype.SetValue = function (index, value)
+    {
+        index = Math.floor(index);
+        
+        if (isNaN(index) || index < 0 || index >= this.list.length)
+            return;
+        
+        if (this.distinct && contains(this.list, value))
+            return;
+
+        this.list[index] = value;
+    };
+    
+    Acts.prototype.Push = function (where, value)
+    {
+        if (this.distinct && contains(this.list, value))
+            return;
+        
+        if (where === 0)	// back
+        {
+            this.list.push(value);
+        }
+        else				// front
+        {
+            this.list.unshift(value);
+        }
+    };
+    
+    Acts.prototype.Pop = function (where)
+    {
+        if (where === 0)	// back
+        {
+            this.list.pop();
+        }
+        else				// front
+        {
+            this.list.shift();
+        }
+    };
+    
+    Acts.prototype.Clear = function ()
+    {
+        this.current = 0;
+        for (var index = 0; index < this.list.length; index++)
+            this.list[index] = 0;
+    };
+    
+    Acts.prototype.SetDistinct = function (allowRepeated)
+    {
+        this.distinct = allowRepeated === 0;
+        
+        var previousList = this.list;
+        
+        this.list = [];
+        
+        for (var index in previousList){
+            this.Push(previousList[index]);
+        }
+        
+        if(this.current >= this.list.length){
+            if(this.list.length > 0){
+                this.current = this.list.length - 1;
+            }else
+            {
+                this.current = 0;
+            }
+        }
+    };
+    
+    function compareValues(va, vb)
+    {
+        // Both numbers: compare as numbers
+        if (cr.is_number(va) && cr.is_number(vb))
+            return va - vb;
+        // Either is a string: compare as strings
+        else
+        {
+            var sa = "" + va;
+            var sb = "" + vb;
+            
+            if (sa < sb)
+                return -1;
+            else if (sa > sb)
+                return 1;
+            else
+                return 0;
+        }
+    };
+    
+    function contains(array, value){
+        for(var index in array)
+        {
+            var arrayValue = array[index];
+            if(compareValues(arrayValue, value) === 0)
+                return true;
+        }
+        
+        return false;
+    };
+    
+    Acts.prototype.Sort = function ()
+    {
+        this.list.sort(compareValues);
+    };
+    
+    // ... other actions here ...
+    
+    pluginProto.acts = new Acts();
+    
+    //////////////////////////////////////
+    // Expressions
+    function Exps() {};
+    
+    // the example expression
+    Exps.prototype.Current = function (ret)
+    {
+        ret.set_any(this.list[this.current]);
+    };
+    
+    Exps.prototype.PeekNext = function (ret)
+    {
+        ret.set_any(this.list[this.nextPosition()]);
+    };
+    
+    Exps.prototype.PeekPrevious = function (ret)
+    {
+        ret.set_any(this.list[this.previousPosition()]);
+    };
+
+    Exps.prototype.Size = function (ret)
+    {
+        ret.set_int(this.list.length);
+    };
+
+    Exps.prototype.Front = function (ret)
+    {
+        ret.set_int(this.list[0]);
+    };
+
+    Exps.prototype.Back = function (ret)
+    {
+        ret.set_int(this.list[this.list.length - 1]);
+    };
+    
+    // ... other expressions here ...
+    
+    pluginProto.exps = new Exps();
+
+}());
